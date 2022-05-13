8517f48250f9e65ef012dd9a285c45f183155dc8
javiermarin24 <>
2022-05-13 15:23:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -142,10 +142,9 @@ List* get_adj_nodes(Node* n){
             }
               
           }
-          
-        }
-        n->sudo[i][j] =0;
+          n->sudo[i][j] =0;
           return list;
+        }
 
       }
       
